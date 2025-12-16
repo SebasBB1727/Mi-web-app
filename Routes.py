835e6c9eb1c801ef3592f172d55ea4d065e18990
Sebastian Galicia <galiciavega1im12@gmail.com>
2025-12-15 18:47:01 -0600

    Se creo el programa de rutas

---
+++ Routes.py
@@ -0,0 +1,14 @@
+# Este es el apartado de las Rutas
+from Resources import *
+#Importamos los recursos de nuestro otro documento (Resources)
+#para poder ocupar las clases que tenemos en ese archivo
+
+
+#Le damos como argumento "api" para que en el documento app pueda ejecutarse sin problemas
+def crear_rutas(api):
+
+    #Lo que hace esto es ejecutar un recurso en especifico en la ruta definida "/hola"
+    api.add_resource(HolaMundo, '/hola')
+
+    api.add_resource(PantallaInicio, '/')
+    #EL "/" define la ruta de incio en los sitios web
