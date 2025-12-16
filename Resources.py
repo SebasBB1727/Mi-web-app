42be885633bbaaf6c3d80344bab4f628a96efec8
Sebastian Galicia <galiciavega1im12@gmail.com>
2025-12-15 20:29:53 -0600

    Actualizacion de recursos

--- Resources.py
+++ Resources.py
@@ -3,12 +3,24 @@ from flask_restful import Resource
 from flask import make_response, render_template
 #Definimos una clase tipo flask que nos permita devolver un recurso
 #dicho recursos sera aquel que nosotros definamos como programadores
-class HolaMundo(Resource):
-    def get(self):
-        return{"Hola" : "mundo"}
+
 class PantallaInicio(Resource):
     def get(self):
         #Renderizamos el contenido en un html
         contenido = render_template("index.html")
         #Retornamos el contenido renderizado en una respuesta
+        return make_response(contenido)
+
+class Login(Resource):
+    def get(self):
+
+        contenido = render_template("login.html")
+
+        return make_response(contenido)
+
+class Signup(Resource):
+    def get(self):
+
+        contenido = render_template("Signup.html")
+
         return make_response(contenido)
