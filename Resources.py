94b62951b51eaaf32fe948e02d468a4cdb89a2c4
Sebastian Galicia <galiciavega1im12@gmail.com>
2025-12-30 11:56:47 -0600

    Actualizacion de la Resources

--- Resources.py
+++ Resources.py
@@ -1,6 +1,7 @@
 #Esta es el apartado de los recursos
 from flask_restful import Resource
-from flask import make_response, render_template
+from extensiones import inicializar_db
+from flask import make_response, render_template, request
 #Definimos una clase tipo flask que nos permita devolver un recurso
 #dicho recursos sera aquel que nosotros definamos como programadores
 
@@ -11,6 +12,7 @@ class PantallaInicio(Resource):
         #Retornamos el contenido renderizado en una respuesta
         return make_response(contenido)
 
+
 class Login(Resource):
     def get(self):
 
@@ -18,9 +20,26 @@ class Login(Resource):
 
         return make_response(contenido)
 
+    #Aqui haremos una funcion con metodo post para poder recibir informacion
+    def post(self):
+        #aqui al recibir un post agregamos como diccionario los datos que reciba
+        print("se ha recibido una peticion")
+        datos = request.form.to_dict()
+        print(datos)
+        #aqui generamos una condicion que si coinciden las contraseñas arroja otra pagina que es la de inicio de sesion
+        if datos["correo"] == "kiubo@correo.com" and datos["password"] == "123456":
+            return make_response(render_template("sesioniniciada.html"))
+
 class Signup(Resource):
     def get(self):
 
         contenido = render_template("Signup.html")
 
         return make_response(contenido)
+    def post(self):
+        print("se ha recibido una peticion")
+        datos = request.form.to_dict()
+        print(datos)
+        #aqui generamos una condicion que si coinciden las contraseñas arroja otra pagina que es la de inicio de sesion
+        if datos["passwordconfirmed"] == datos["password"]:
+            return make_response(render_template("sesioniniciada.html"))
