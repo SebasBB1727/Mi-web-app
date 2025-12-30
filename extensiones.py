e16086a364662a9b669fc7b62c94c6a968b7e21c
Sebastian Galicia <galiciavega1im12@gmail.com>
2025-12-30 12:12:01 -0600

    Comentario en extensiones.py

--- extensiones.py
+++ extensiones.py
@@ -1,5 +1,5 @@
 from supabase import create_client
-
+#Creamos el enlace con la base de datos en supabase
 def inicializar_db():
     client = create_client("https://wkcpobfnenbjmzzwptyd.supabase.co","SupabaseHybridge2028")
     return client
